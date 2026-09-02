34bf5c876837b2911aa40b23367576845241fe8e
jerry200176-png <jerry200176@gmail.com>
2026-09-02 23:00:02 +0800

    fix(schedule): simplify slot subset guard

--- backend/app/Http/Controllers/StudentClassController.php
+++ backend/app/Http/Controllers/StudentClassController.php
@@ -6108,7 +6108,7 @@ class StudentClassController extends Controller
         $oldKeys = array_values(array_unique(array_map($slotKey, $previousSlots)));
         $newKeys = array_values(array_unique(array_map($slotKey, $newSlots)));
 
-        return $newKeys !== [] && array_diff($newKeys, $oldKeys) === [];
+        return count(array_diff($newKeys, $oldKeys)) === 0;
     }
 
     /**
